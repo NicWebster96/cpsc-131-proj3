08149b3a1b1d3b9c80171c4772b29a1ae87e739e
Nicholas Webster <NicWebster96@csu.fullerton.edu>
2018-10-23 23:15:59 -0700

    yea

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Project 2
+# Project 3
 ## CSUF CPSC 131, Fall 2018
 
 MUST EDIT WITH YOUR OWN NAME AND EMAIL IN THE SAME FORMAT
 
 Group members:
-- Kenneth Aguilar calstateken@csu.fullerton.edu
 - Nicholas Webster NicWebster96@csu.fullerton.edu
--- minideque.h
+++ minideque.h
@@ -11,7 +11,7 @@
 #include <algorithm>
 #include <string>
 #include <iostream>
-
+#include <math.h>
 
 template <typename T>
 class minideque {
@@ -25,30 +25,91 @@ public:
   minideque& operator=(const minideque<T>& other) = default;
   ~minideque() = default;
 
-  size_t size() const;             // TODO
-  size_t fronthalfsize() const;    // TODO
-  size_t backhalfsize() const;     // TODO
-  bool empty()  const;              // TODO
+  size_t size() const { return fronthalf_.size() + backhalf_.size(); }           
+  size_t fronthalfsize() const { return fronthalf_.size(); }   
+  size_t backhalfsize() const { return backhalf_.size(); }    
+  bool empty()  const { return size() == 0; }
 
   // balance queue across both vectors if pop_front/back is requested on an empty vector
   // e.g., minideque has this:                  | ABCDEFG
   // after pop_front                        BCD | EFG (A discarded)
   // symmetric logic for converse case: ABCDEFG | ===> ABC | DEF (G discarded) after pop_back
-  void pop_front();  // TODO
-  void pop_back();   // TODO
+  void pop_front() {
+	  if (empty()) { throw std::range_error("popping empty queue"); }
+	  if ((fronthalfsize() == 0) && (backhalfsize() != 0)) {
+		  for (int i = 0; i < ceil((backhalfsize() / 2)); i++) {
+			  fronthalf_.push_back(backhalf_[0]);
+              backhalf_.erase(0);
+		  }
+	  }
+	  fronthalf_.pop_back();
+  }
 
-  void push_front(const T& value);   // TODO
-  void push_back(const T& value);    // TODO
+  void pop_back() {
+	  if (empty()) { throw std::range_error("popping empty queue"); }
+	  if ((fronthalfsize() != 0) && (backhalfsize() == 0)) {
+		  for (int i = 0; i < ceiling((fronthalfsize() / 2)); i++) {
+			  backhalf_.push_back(fronthalf_[0]);
+			  fronthalf_.erase(0);
+		  }
+	  }
+	  backhalf_.pop_back();
+  }
 
-  const T& front() const;   // TODO
-  const T& back() const;    // TODO
-  T& front();               // TODO
-  T& back();                // TODO
+  void push_front(const T& value) {
+	  fronthalf_.push_back(value);
+  }
+  void push_back(const T& value) {
+	  backhalf_.push_back(value);
+  }
 
-  const T& operator[](size_t index) const;  // TODO
-  T& operator[](size_t index);              // TODO
+  const T& front() const {
+	  if (fronthalf_.empty()) { return backhalf_.front(); }
+	  else {
+		return fronthalf_.back();
+	  }
+  }
+  const T& back() const {
+	  if (fronthalf_.empty()) { return fronthalf_.front(); }
+	  else {
+		  return backhalf_.back();
+	  }
+  }
 
-  void clear();  // TODO
+  T& front() {
+	  if (fronthalf_.empty()) { return backhalf_.front(); }
+	  else {
+		  return fronthalf_.back();
+	  }
+  }
+  T& back() {
+	  if (fronthalf_.empty()) { return fronthalf_.front(); }
+	  else {
+		  return backhalf_.back();
+	  }
+  }
+
+  const T& operator[](size_t index) const {
+	  if (index > fronthalfsize()) {
+          return backhalf_.at(index - fronthalfsize() - 1);
+	  }
+	  else {
+		  return fronthalf_.at(fronthalfsize() - index - 1);
+	  }
+  }
+  T& operator[](size_t index) {
+	  if (index > fronthalfsize()) {
+          return backhalf_.at(index - fronthalfsize() - 1);
+	  }
+	  else {
+		  return fronthalf_.at(fronthalfsize() - index - 1);
+	  }
+  }
+
+  void clear() {
+	  fronthalf_.clear;
+	  backhalf_.clear;
+  }
 
   friend std::ostream& operator<<(std::ostream& os, const minideque<T>& dq) {    // do not change
     if (dq.empty()) { return os << "minideque is empty"; }
